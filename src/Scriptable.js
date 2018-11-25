f5a0614608d57019ffcfb12f1672a6e711025aa4
Robert <rlamacraft@gmail.com>
2018-11-25 13:22:06 +0000

    Clean-up commented out JS code

--- src/Scriptable.js
+++ src/Scriptable.js
@@ -2,45 +2,6 @@
 
 exports.argsText = args.plainTexts[0];
 
-// function listOfButtons(buttons) {
-//   if(Object.keys(buttons).length === 0) {
-//     return [];
-//   } else {
-//     const list = listOfButtons(buttons["value1"]);
-//     list.push(buttons["value0"]);
-//     return list;
-//   }
-// }
-//
-// function addButtons(buttons, alert) {
-//   return listOfButtons(buttons).reduce(function(acc, btn) {
-//     acc.addAction(btn);
-//     return acc;
-//   }, alert);
-// }
-//
-// function showAlert(pureAlert) {
-//   const alert = new Alert();
-//   if(pureAlert["message"] !== {}) {
-//     alert.message = pureAlert["message"]["value0"]
-//   }
-//   if(pureAlert["title"] !== {}) {
-//     alert.title = pureAlert["title"]["value0"]
-//   }
-//   addButtons(pureAlert["buttons"], alert);
-//   return alert.presentAlert().then(function(actionIndex) {
-//     return listOfButtons(pureAlert["buttons"])[actionIndex];
-//   });
-// }
-//
-// exports.presentAlertImpl = function(pureAlert) {
-//   return function() {
-//     // return function(foo) {
-//       return showAlert(pureAlert);
-//     // }
-//   }
-// }
-
 function listOfButtons(buttons) {
   if(Object.keys(buttons).length === 0) {
     return [];
