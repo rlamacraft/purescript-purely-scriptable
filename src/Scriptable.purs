6d9550558ae1c1c88394ba6fcd2c9c481ea6ef50
Robert <rlamacraft@gmail.com>
2018-12-01 18:48:10 +0000

    Remove root package

--- src/Scriptable.purs
+++
@@ -1,4 +0,0 @@
-module PurelyScriptable (newAlert, presentAlert, setMessage, setTitle, addAction, Alert, Button(..), argsText) where
-
-import PurelyScriptable.Args(argsText)
-import PurelyScriptable.Alert(newAlert, presentAlert, setMessage, setTitle, addAction, Alert, Button(..))
