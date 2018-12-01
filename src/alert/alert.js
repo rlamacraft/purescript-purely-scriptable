006fd26a954a1ae20065a2c683ed887b810358af
Robert <rlamacraft@gmail.com>
2018-12-01 21:07:03 +0000

    Fix typo in showAlert's invocation of addTextFields

--- src/alert/alert.js
+++ src/alert/alert.js
@@ -33,7 +33,7 @@ function showAlert(btnType, pureAlert) {
     alert.title = pureAlert["title"]["value0"]
   }
   addButtons(btnType, pureAlert["btnLabels"], alert);
-  addTextFields(purAlert["textFields"], alert);
+  addTextFields(pureAlert["textFields"], alert);
   return alert.presentAlert().then(function(actionIndex) {
     return purescriptListToJsArray(pureAlert["buttons"])[actionIndex];
     //TODO: Also return values of text fields
