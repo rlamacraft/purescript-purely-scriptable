b7bfe2c886c9968f5d69b50cf56792286fec4367
Robert <rlamacraft@gmail.com>
2018-12-01 21:29:01 +0000

    Fix variable location in build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,5 @@
-final_location = ~/Library/Mobile\ Documents/iCloud~dk~simonbs~Scriptable/Documents/$1.js
+LOCATION="$HOME/Library/Mobile\ Documents/iCloud~dk~simonbs~Scriptable/Documents/$1.js"
+echo $LOCATION
 pulp build -O --to output.js
-printf '%b\n%s\n' "// Variables used by Scriptable.\n// These must be at the very top of the file. Do not edit.\n// icon-color: blue; icon-glyph: magic; share-sheet-inputs: plain-text;\nfunction setTimeout(f, _){f();}" "$(cat output.js)" > $final_location
-echo "Finished compiling to $final_location"
+printf '%b\n%s\n' "// Variables used by Scriptable.\n// These must be at the very top of the file. Do not edit.\n// icon-color: blue; icon-glyph: magic; share-sheet-inputs: plain-text;\nfunction setTimeout(f, _){f();}" "$(cat output.js)" > ~/Library/Mobile\ Documents/iCloud~dk~simonbs~Scriptable/Documents/$1.js
+echo "Finished compiling to $LOCATION"
