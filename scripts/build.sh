c542dcbf0b511973f8f8c0029e5f7755cbbf7802
Robert <rlamacraft@gmail.com>
2018-12-01 21:10:40 +0000

    Add done message to build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,1 +1,4 @@
-pulp build -O --to output.js ; printf '%b\n%s\n' "// Variables used by Scriptable.\n// These must be at the very top of the file. Do not edit.\n// icon-color: blue; icon-glyph: magic; share-sheet-inputs: plain-text;\nfunction setTimeout(f, _){f();}" "$(cat output.js)" > ~/Library/Mobile\ Documents/iCloud~dk~simonbs~Scriptable/Documents/$1.js
+final_location = ~/Library/Mobile\ Documents/iCloud~dk~simonbs~Scriptable/Documents/$1.js
+pulp build -O --to output.js
+printf '%b\n%s\n' "// Variables used by Scriptable.\n// These must be at the very top of the file. Do not edit.\n// icon-color: blue; icon-glyph: magic; share-sheet-inputs: plain-text;\nfunction setTimeout(f, _){f();}" "$(cat output.js)" > $final_location
+echo "Finished compiling to $final_location"
