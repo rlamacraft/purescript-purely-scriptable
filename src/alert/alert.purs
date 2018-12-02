f0f8f95fa59f120b5ad0d13117e8f5a8c512a1c8
Robert <rlamacraft@gmail.com>
2018-12-02 16:28:45 +0000

    Further cleanup

--- src/alert/alert.purs
+++ src/alert/alert.purs
@@ -1,17 +1,15 @@
 module PurelyScriptable.Alert (newAlert, presentAlert, setMessage, setTitle, addAction, Alert, Button(..), TextField(..), addTextField, AlertResult(..)) where
 
-import Control.Bind ((>>=))
-import Control.Promise (Promise, toAff)
+import Control.Promise (Promise, toAffE)
+import Control.Semigroupoid ((>>>))
 import Data.List (List(Nil), (:))
 import Data.Maybe (Maybe(..))
-import Data.Semigroup ((<>))
 import Data.Show (class Show, show)
 import Effect (Effect)
 import Effect.Aff (Aff)
-import Effect.Class (liftEffect)
 
 presentAlert :: forall b . Show b => Alert b -> Aff (AlertResult b)
-presentAlert alert = liftEffect (presentAlertImpl alert) >>= toAff
+presentAlert = presentAlertImpl >>> toAffE
 
 data AlertResult b = Result (Button b) (List String)
 
@@ -19,24 +17,27 @@ foreign import presentAlertImpl :: forall b . Show b => Alert b -> Effect (Promi
 
 newtype Button b = Button b
 
-type Alert b = { title :: Maybe String,
-                       message :: Maybe String,
-                       buttons :: List (Button b),
-                       btnLabels :: List String,
-                       textFields :: List TextField
-                     }
+type Alert b = {
+  title :: Maybe String,
+  message :: Maybe String,
+  buttons :: List (Button b),
+  btnLabels :: List String,
+  textFields :: List TextField
+}
 
-type TextField = { placeholder :: Maybe String,
-                   text :: Maybe String
-                 }
+type TextField = { 
+  placeholder :: Maybe String,
+  text :: Maybe String
+}
 
 newAlert :: forall b . Alert b
-newAlert = { message : Nothing,
-             title : Nothing,
-             buttons : Nil,
-             btnLabels : Nil,
-             textFields : Nil
-           }
+newAlert = {
+  message : Nothing,
+  title : Nothing,
+  buttons : Nil,
+  btnLabels : Nil,
+  textFields : Nil
+}
 
 setMessage :: forall b . String -> Alert b -> Alert b
 setMessage msg alert = alert { message = Just msg }
