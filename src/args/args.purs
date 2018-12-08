e450e2e6a99931c310a8d40af663e07b475268b8
Robert <rlamacraft@gmail.com>
2018-12-08 14:35:30 +0000

    Implementation of Ask for ArgsText

--- src/args/args.purs
+++ src/args/args.purs
@@ -1,5 +1,11 @@
-module PurelyScriptable.Args (ArgsText, argsText) where
+module PurelyScriptable.Args (ArgsText(..), argsText) where
+
+import Data.Functor ((<#>))
+import PurelyScriptable.Alert (class Ask, ask)
 
 newtype ArgsText = ArgsText String
 
 foreign import argsText :: Array ArgsText
+
+instance askArgsText :: Ask ArgsText where
+  ask alertTitle = ask alertTitle <#> ArgsText
