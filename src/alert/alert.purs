3a9d4de15cc663cc1bfee1829bb94599f9628551
Robert <rlamacraft@gmail.com>
2018-12-08 15:07:18 +0000

    Helpers section, with displayString

--- src/alert/alert.purs
+++ src/alert/alert.purs
@@ -1,13 +1,13 @@
 module PurelyScriptable.Alert (newAlert, presentAlert, setMessage, setTitle, addAction, addActions, Alert,
                                Button(..), TextField(..), addTextField, AlertResult(..), textFieldValue,
-                               unsafeTextFieldValue, class Ask, ask, askIfNothing, present, closeButton) where
+                               unsafeTextFieldValue, class Ask, ask, askIfNothing, present, closeButton, displayString, Close(..)) where
 
 import Control.Applicative (pure)
 import Control.Promise (Promise, toAffE)
 import Control.Semigroupoid ((>>>))
 import Data.Array (index, unsafeIndex)
 import Data.Foldable (foldr)
-import Data.Function (flip, (#), ($), (<<<))
+import Data.Function (flip, (#), ($))
 import Data.Functor ((<#>))
 import Data.List (List(..), (:))
 import Data.Maybe (Maybe(..), maybe)
@@ -77,14 +77,8 @@ unsafeTextFieldValue :: forall b . Partial => Int -> AlertResult b -> String
 unsafeTextFieldValue i (Result _ textFieldValues) = unsafeIndex textFieldValues i
 
 -----------------------
--- Ask
+-- Helpers
 -----------------------
-
-class Ask a where
-  ask :: String -> Aff a
-
-askIfNothing :: forall a . Ask a => String -> Maybe a -> Aff a
-askIfNothing alertTitle = maybe (ask alertTitle) pure
 
 data Close = Close
 
@@ -93,6 +87,21 @@ instance showClose :: Show Close where
 
 closeButton :: Button Close
 closeButton = Button Close
+
+displayString :: String -> Aff (AlertResult Close)
+displayString str = newAlert # setTitle str
+                    >>> addAction closeButton
+                    >>> presentAlert
+
+-----------------------
+-- Ask
+-----------------------
+
+class Ask a where
+  ask :: String -> Aff a
+
+askIfNothing :: forall a . Ask a => String -> Maybe a -> Aff a
+askIfNothing alertTitle = maybe (ask alertTitle) pure
 
 instance askString :: Ask String where
   ask alertTitle = newAlert # setTitle alertTitle
