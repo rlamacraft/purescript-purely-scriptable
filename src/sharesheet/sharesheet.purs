2da833cd75803316f938e6ad606aa9efa0ee1d4d
Robert <rlamacraft@gmail.com>
2018-12-16 22:21:28 +0000

    Correct foreign use of Aff a to Effect (Promise a)

--- src/sharesheet/sharesheet.purs
+++ src/sharesheet/sharesheet.purs
@@ -1,19 +1,22 @@
 module ShareSheet (ShareSheetResult, presentAndWait) where
 
+import Control.Promise (Promise, toAffE)
+import Data.Function ((#))
 import Data.Functor ((<#>))
+import Effect (Effect)
 import Effect.Aff (Aff)
 
 data ShareSheetResult = Result Boolean
 
 presentAndWait :: forall a . a -> Aff ShareSheetResult
-presentAndWait activityItems = presentAndWaitImpl activityItems <#> transformShareSheetResult
+presentAndWait activityItems = presentAndWaitImpl activityItems # toAffE <#> transformShareSheetResult
 
 type ForeignShareSheetResult = {
   activity_type :: String,
   completed :: Boolean
 }
 
-foreign import presentAndWaitImpl :: forall a . a -> Aff ForeignShareSheetResult
+foreign import presentAndWaitImpl :: forall a . a -> Effect (Promise ForeignShareSheetResult)
 
 transformShareSheetResult :: ForeignShareSheetResult -> ShareSheetResult
 transformShareSheetResult foreignResult = Result foreignResult.completed
