7065dcb7c23fa4ae5e4317df21a72dc89c62284d
Robert <rlamacraft@gmail.com>
2018-12-21 18:08:54 +0000

    Remove old Scriptable APIs
    'presentAndWait' has been removed and 'present' is now asynchronous

--- src/sharesheet/sharesheet.js
+++ src/sharesheet/sharesheet.js
@@ -1,19 +1,5 @@
 "use strict";
 
-function presentAndWait(activityItems) {
-    return function() {
-	ShareSheet.presentAndWait(activityItems);
-    }
-}
-
-exports.presentAndWaitImpl_single = function(activityItem) {
-    return presentAndWait([activityItem]);
-}
-
-exports.presentAndWaitImpl_multiple = function(activityItems) {
-    return presentAndWait(activityItems);
-}
-
 function present(activityItems) {
     ShareSheet.present(activityItems);
 }
--- src/sharesheet/sharesheet.purs
+++ src/sharesheet/sharesheet.purs
@@ -1,36 +1,18 @@
-module ShareSheet (ShareSheetResult, presentAndWait, presentAndWaitMultiple, present, presentMultiple) where
+module ShareSheet (Activity, present, presentMultiple) where
 
 import Control.Promise (Promise, toAffE)
 import Control.Semigroupoid ((>>>))
-import Data.Unit(Unit)
 import Effect (Effect)
 import Effect.Aff (Aff)
 
-type ShareSheetResult = {
-  activity_type :: String,
-  completed :: Boolean
-}
+newtype Activity = Activity String
 
------------------------
--- ShareSheet API
------------------------
+present :: forall a . a -> Aff Activity
+present = presentImpl_single >>> toAffE
 
-presentAndWait :: forall a . a -> Aff ShareSheetResult
-presentAndWait = presentAndWaitImpl_single >>> toAffE
+foreign import presentImpl_single :: forall a . a -> Effect (Promise Activity)
 
-foreign import presentAndWaitImpl_single :: forall a . a -> Effect (Promise ShareSheetResult)
+presentMultiple :: forall a . Array a -> Aff Activity
+presentMultiple = presentImpl_multiple >>> toAffE
 
-presentAndWaitMultiple :: forall a . Array a -> Aff ShareSheetResult
-presentAndWaitMultiple = presentAndWaitImpl_multiple >>> toAffE
-
-foreign import presentAndWaitImpl_multiple :: forall a . Array a -> Effect (Promise ShareSheetResult)
-
-present :: forall a . a -> Effect Unit
-present = presentImpl_single
-
-foreign import presentImpl_single :: forall a . a -> Effect Unit
-
-presentMultiple :: forall a . Array a -> Effect Unit
-presentMultiple = presentImpl_multiple
-
-foreign import presentImpl_multiple :: forall a . Array a -> Effect Unit
+foreign import presentImpl_multiple :: forall a . Array a -> Effect (Promise Activity)
