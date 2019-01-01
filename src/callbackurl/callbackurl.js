8099f56291e49c46a8257983347c2b4afefbf7d3
Robert <rlamacraft@gmail.com>
2019-01-01 20:34:39 +0000

    Finalised JS implementation of open

--- src/callbackurl/callbackurl.js
+++ src/callbackurl/callbackurl.js
@@ -1,12 +1,11 @@
 "use strict";
 
-exports.openImpl = function() {
-    return function(c) {
-	const URL = new CallbackURL(c.target + "://x-callback-url/" + c.action);
-	console.log(URL);
-	console.log(c);
-	return function() {
-	    return Promise.reject("developing");
-	}
+exports.openImpl = function(c) {
+    const url = c.parameters.reduce(function(acc, param) {
+	acc.addParameter(param.value0, param.value1);
+	return acc;
+    }, new CallbackURL(c.target + "://x-callback-url/" + c.action));
+    return function() {
+	return url.open();
     }
 }
