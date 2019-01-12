1f7ee7d32c2c05f510ed26a0b8a2336e0fcf2e02
Robert <rlamacraft@gmail.com>
2019-01-12 15:46:33 +0000

    Colors

---
+++ src/color/color.js
@@ -0,0 +1,61 @@
+"use strict";
+
+exports.black_Impl = function() {
+    return Color.black();
+}
+
+exports.blue_Impl = function() {
+    return Color.blue();
+}
+
+exports.brown_Impl = function() {
+    return Color.brown();
+}
+
+exports.clear_Impl = function() {
+    return Color.clear();
+}
+
+exports.cyan_Impl = function() {
+    return Color.cyan();
+}
+
+exports.darkGray_Impl = function() {
+    return Color.darkGray();
+}
+
+exports.gray_Impl = function() {
+    return Color.gray();
+}
+
+exports.green_Impl = function() {
+    return Color.green();
+}
+
+exports.lightGray_Impl = function() {
+    return Color.lightGray();
+}
+
+exports.magenta_Impl = function() {
+    return Color.magenta();
+}
+
+exports.orange_Impl = function() {
+    return Color.orange();
+}
+
+exports.purple_Impl = function() {
+    return Color.purple();
+}
+
+exports.red_Impl = function() {
+    return Color.red();
+}
+
+exports.white_Impl = function() {
+    return Color.white();
+}
+
+exports.yellow_Impl = function() {
+    return Color.yellow();
+}
